2b4a8500766f06a41e2fdc84d9c1894c69244c22
abinladin <abdullahbinladin98@gmail.com>
2019-04-22 23:40:18 +0200

    refactored for better quality

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,11 +1,11 @@
 import numpy as np
-
+import os
 class Neural_Network:
 
     def __init__(self, hidden_layer_size):
         # Layers
         self.input_layer_size = 2
-        self.hidden_layer_size = hidden_layer_size
+        self.hidden_layer_size = hidden_layer_size + 1
         self.output_layer_size = 1
 
         # Weights
@@ -28,7 +28,7 @@ class Neural_Network:
         return self.a_L_one
     
     def backpropogate(self, X, y, o):
-        self.o_error = y - o
+        self.o_error = (y - o)
         self.o_delta = self.o_error * self.d_sigmoid(o)
 
         self.z2_error = self.o_delta.dot(self.weights_hidden_to_output.T)
@@ -44,16 +44,8 @@ class Neural_Network:
     def predict(self):
         print("Predicted data based on weights:")
         print("Input: \n" + str(XOR_gate_inputs))
-        print("output: \n" + str(self.feed_forward(XOR_gate_inputs)))
+        print("predicted output: \n" + str(self.feed_forward(XOR_gate_inputs)))
 
 # Training data
 XOR_gate_inputs = np.array( ([0, 0], [0, 1], [1, 0], [1, 1]), dtype=float)
 XOR_gate_outputs = np.array( ([0], [1], [1], [0]), dtype=float)
-
-neural_network = Neural_Network(20)
-neural_network_output = neural_network.feed_forward(XOR_gate_inputs)
-
-for i in range(500):
-    neural_network.train(XOR_gate_inputs, XOR_gate_outputs)
-
-neural_network.predict()
